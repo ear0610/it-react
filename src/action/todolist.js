25a138b6c01fb7659488d4fb6d4a781be537728b
ear0610 <ear780610@gmail.com>
2022-11-30 23:44:28 +0800

    add properties

--- src/action/todolist.js
+++
@@ -1,8 +0,0 @@
-export const ADD_TODO = 'ADD_TODO';
-
-const addTodo = todo => ({
-  type: ADD_TODO,
-  payload: {
-    todo,
-  },
-});
--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import todoReducer from '../reducer/todolist';
-
-const store = createStore(todoReducer);
-
-export default store;
